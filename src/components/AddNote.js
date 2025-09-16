18b32cffee48b4da04a44ec4aaa263a8de9794d6
Disha Sahu <sahu.disha21@gmail.com>
2025-09-17 00:28:56 +0530

    Add and delete a note from UI TODO Api Call

---
+++ src/components/AddNote.js
@@ -0,0 +1,37 @@
+import React, { useContext, useState } from 'react'
+import NoteContext from '../context/notes/NoteContext';
+
+const AddNote = () => {
+    const context = useContext(NoteContext);
+    const { addNote } = context;
+    const [note, setNote] = useState({ title: "", description: '', tag: '' })
+    const onChange = (e) => {
+        setNote({ ...note, [e.target.name]: e.target.value })
+    }
+    const handleClick = (e) => {
+        e.preventDefault();
+        addNote(note.title, note.description, note.tag);
+    }
+    return (
+        <div className="container my-3">
+            <h2>Add a Note</h2>
+            <form className='my-3'>
+                <div className="form-group">
+                    <label htmlFor="title">Title</label>
+                    <input type="text" className="form-control" id="title" name='title' placeholder="Enter Title" onChange={onChange} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="description">Description</label>
+                    <input type="text" className="form-control" id="description" name='description' placeholder="Enter Description" onChange={onChange} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="tag">Tag</label>
+                    <input type="text" className="form-control" id="tag" name='tag' placeholder="Enter Tag" onChange={onChange} />
+                </div>
+                <button type="submit" className="btn btn-primary my-2" onClick={handleClick}>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default AddNote
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import noteContext from "./noteContext";
+import NoteContext from "./NoteContext";
 
 
 const NoteState = (props) => {
@@ -21,7 +21,7 @@ const NoteState = (props) => {
             "tag": "personal",
             "date": "2025-09-11T07:51:59.196Z",
             "__v": 0
-        },{
+        }, {
             "_id": "68ba9920f58c9669b5144bc3",
             "user": "68b94d6acb6bcfad6e51aef2",
             "title": "I am title",
@@ -38,7 +38,7 @@ const NoteState = (props) => {
             "tag": "personal",
             "date": "2025-09-11T07:51:59.196Z",
             "__v": 0
-        },{
+        }, {
             "_id": "68ba9920f58c9669b5144bc5",
             "user": "68b94d6acb6bcfad6e51aef2",
             "title": "I am title",
@@ -55,7 +55,7 @@ const NoteState = (props) => {
             "tag": "personal",
             "date": "2025-09-11T07:51:59.196Z",
             "__v": 0
-        },{
+        }, {
             "_id": "68ba9920f58c9669b5144bc7",
             "user": "68b94d6acb6bcfad6e51aef2",
             "title": "I am title",
@@ -75,10 +75,38 @@ const NoteState = (props) => {
         }
     ]
     const [notes, setNotes] = useState(notesInitial)
+
+    //Add a note
+    const addNote = (title, description, tag) => {
+        //ToDO API Calls
+        console.log('Adding a note');
+        const note = {
+            "_id": "68c27f9f769c9cc4f7a139d8",
+            "user": "68b94d6acb6bcfad6e51aef2",
+            "title": title,
+            "description": description,
+            "tag": tag,
+            "date": "2025-09-11T07:51:59.196Z",
+            "__v": 0
+        }
+        setNotes(notes.concat(note));
+    }
+    //Delete a note
+    const deleteNote = (id) => {
+        //TODO API Call 
+        console.log('Deleting a note with id ' + id)
+        const newNotes = notes.filter((note) => { return note._id !== id })
+        setNotes(newNotes);
+    }
+    //Edit a note
+    const editNote = () => {
+
+    }
+
     return (
-        <noteContext.Provider value={{ notes, setNotes }}>
+        <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
             {props.children}
-        </noteContext.Provider>
+        </NoteContext.Provider>
     )
 }
 
